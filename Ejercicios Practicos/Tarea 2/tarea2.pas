13eca218baa9645d18647eeb4184881560002f84
Maximiliano Rodriguez <55286302+MaxiRod30@users.noreply.github.com>
2020-03-07 09:27:29 -0300

    Add files via upload

---
+++ Ejercicios Practicos/Tarea 2/tarea2.pas
@@ -0,0 +1,42 @@
+
+{ InCo - Programación 1 }
+{ Laboratorio 2019      }
+
+{ esqueleto de archivo tarea2.pas a ser entregado}
+
+
+function esPrefijo (pal1,pal2 : TipoPalabra) : boolean;
+begin
+end;
+
+function igualPalabra (pal1,pal2 : TipoPalabra) : boolean;
+begin
+end;
+
+procedure palabraADistancia
+              ( palabra       : TipoPalabra;
+                vocabulario   : TipoVocabulario;
+                distancia     : TipoDistancia;
+                var resultado : PosiblePalabra);
+begin
+end;
+
+procedure masCercana
+            ( vocabulario : TipoVocabulario;
+              palabra     : TipoPalabra;
+              var resultado : TipoPalabra;
+              var distancia : TipoDistancia);
+begin
+end;
+
+function pertenecePalabra (palabra : TipoPalabra; vocabulario : TipoVocabulario): boolean;
+begin
+end;
+
+function completarPalabra (prefijo : TipoPalabra; vocabulario : TipoVocabulario) : ListaPalabras;
+begin
+end;
+
+function estanTodas( lista : ListaPalabras; vocabulario : TipoVocabulario) : boolean;
+begin
+end;
